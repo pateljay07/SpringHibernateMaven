d357c984c563e29c7ee795dc62b0a90696978d47
YDSWorld <YDSWorld@MAC-PARA-WIN7>
2014-12-16 09:48:53 -0500

    intial commit

---
+++ Spring_Maven_1/WebContent/index.jsp
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>Spring MVC Tutorial Series by Crunchify.com</title>
+
+</head>
+<body>
+	<br>
+	<div align='center'>
+		<h2>
+			Hey You..!! This is your 1st Spring MCV Tutorial..<br> <br>
+		</h2>
+		<h3>
+			<a href="welcome.html">Click here to See Welcome Message... </a>(to
+			check Spring MVC Controller... @RequestMapping("/welcome"))
+			<p>
+				<a href="shipping/orders.html">Click here to See Welcome Message... </a>
+			</p>
+		</h3>
+	</div>
+</body>
+</html>
---
+++ Spring_Maven_1/src/com/ydsworld/controller/ShippingController.java
@@ -0,0 +1,21 @@
+package com.ydsworld.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/shipping")
+public class ShippingController {
+
+	@RequestMapping("/orders")
+	public ModelAndView orders() {
+ 
+		String message = "<br><div align='center'>"
+				+ "<h3>**********Shipping Order List</h3>This message is comming from CrunchifyHelloWorld.java **********<br><br>";
+		return new ModelAndView("orders", "message", message);
+	}
+	
+	
+	
+}
